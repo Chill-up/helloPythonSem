9f7b58af7f19d34cffd3042208635bbe3d4da539
Andrei <apchilap@gmail.com>
2022-10-11 13:50:33 +0600

    add solution fot 1 task of 6 seminar

--- 20221011/000.py
+++ 20221011/000.py
@@ -1,15 +1,29 @@
 # Ввести с клавиатуры 2 числа (int) и вывести в консоль их НОК (наименьшее общее кратное)
 
-number1 = int(input("Введите первое число: "))
-number2 = int(input("Введите второе число: "))
+# Примитивное решение:
 
-max = max(number1, number2)
-print(max)
+# number1 = int(input("Введите первое число: "))
+# number2 = int(input("Введите второе число: "))
 
-if max % number1 == 0 and max % number2 == 0:
-    print(f"Наименьшее общее кратное для {number1} и {number2} = {max} ")
-else:
-    for i in range(max, number1*number2+1):
-        if i % number1 == 0 and i % number2 == 0:
-            print(f"Наименьшее общее кратное для {number1} и {number2} = {i} ")
-            break
+# max = max(number1, number2)
+# print(max)
+
+# if max % number1 == 0 and max % number2 == 0:
+#     print(f"Наименьшее общее кратное для {number1} и {number2} = {max} ")
+# else:
+#     for i in range(max, number1*number2+1):
+#         if i % number1 == 0 and i % number2 == 0:
+#             print(f"Наименьшее общее кратное для {number1} и {number2} = {i} ")
+#             break
+
+# Правильное решение
+
+m = int(input('Введите первое число: '))
+n = int(input('Введите второе число: '))
+
+i = 1
+
+while (min(m, n)*i) % max(m, n):
+    i += 1
+
+print(f'НОК для {m} и {n} будет {min(m,n)*i}')
